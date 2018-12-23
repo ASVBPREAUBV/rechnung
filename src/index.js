93026021b14ad337eb1e6c679d6bc4d9b0489487
ASVBPREAUBV <paul@paul-mayer.eu>
2018-12-23 20:58:31 +0100

    clean up JS, add comments

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
+// external dependencies
 const express = require('express');
 const pug = require('pug');
 const moment = require('moment');
 
+// internal dependenies
 const tools = require('./tools');
 const config = require('./config/config');
 const invoice = require('./config/invoices/customer');
 
+// express setup
 const app = express();
 app.set('views', './src/views');
 app.set('view engine', 'pug');
 
+// local constants
 const PORT = process.env.PORT;
 const HOST = '0.0.0.0';
 const OUTPUT_FOLDER = './public';
 const OUTPUT_FILENAME = `filename_${invoice.customer.project}_${invoice.invoice.no}`;
 const date = moment().format("DD.MM.YYYY");
-
-if (!PORT) throw new Error('process.env.PORT not defined');
+if (!PORT) throw new Error('process.env.PORT not defined. Please define a PORT in the environment variables');
 
 
 app.get('/', (req, res) => {
--- src/tools.js
+++ src/tools.js
@@ -1,15 +1,12 @@
-const phantom = require('phantom');
 const puppeteer = require('puppeteer');
-
-
 
 const create_pdf = async (output_folder, output_filename) => {
     const file_path = `${output_folder}/${output_filename}.pdf`;
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('http://localhost:3000', {waitUntil: 'networkidle2'});
-        await page.pdf({path: file_path});
-        await browser.close();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000', {waitUntil: 'networkidle2'});
+    await page.pdf({path: file_path});
+    await browser.close();
 };
 
 module.exports =
